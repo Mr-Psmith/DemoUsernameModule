ff4f6bf6d2d610477af8c45b05bcf42b53484ea9
Mr-Psmith <psancthus@gmail.com>
2023-04-01 10:49:28 +0000

    added Refs instead of states in UserForms.js

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./UserForm.css";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrepper";
 
 const UserForm = (props) => {
-  const [userName, setUserName] = useState("");
-  const [age, setAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  /* const [userName, setUserName] = useState(""); Taken out when we used ref-s instead
+  const [age, setAge] = useState(""); */
 
   const [error, setError] = useState("");
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    if (userName.trim().length === 0 || age.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
+    /* if (userName.trim().length === 0 || age.trim().length === 0) {  Taken out when we used ref-s instead*/
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       /*  setError(<ErrorModal modalTitle="Soon to be dynamic" modalMessage="its not hardcoded at all" />); good as well*/
       setError({
         title: "Invalid input",
@@ -20,31 +27,31 @@ const UserForm = (props) => {
       });
       return;
     }
-    if (+age < 14) {
+    if (+enteredAge < 14) {
       setError({
         title: "Invalid age",
         message: "Please Fuck off(enter a valid age, no underagers here (>14))",
       });
       return; /* If we leave this out than this will still print an empty message */
     }
-    /* if (userName.trim().length < 2) { isnt working, why???
-        return;
-    } */
-    props.onSaveUserInfo(userName, age);
-    setAge("");
-    setUserName("");
+    props.onSaveUserInfo(enteredName, enteredAge);
+    /* setAge(""); Taken out when we used ref-s instead
+    setUserName(""); 
+    Instead we use what we shouldnt mopstly but its okay sometimes: */
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const modalCloseHandler = () => { 
     setError(""); /* or null, or anything false, or falsy value */
   };
 
-  const userNameInputHandler = (event) => {
+  /* const userNameInputHandler = (event) => { Taken out when we used ref-s instead
     setUserName(event.target.value);
   };
   const ageInputHandler = (event) => {
     setAge(event.target.value);
-  };
+  }; */
 
   return (
     <Wrapper>
@@ -59,18 +66,20 @@ const UserForm = (props) => {
         <form className="form-control" onSubmit={formSubmitHandler}>
           <label htmlFor="username">Username</label>
           <input
-            value={userName}
+            /* onChange={userNameInputHandler} Taken out when we used ref-s instead
+            value={userName} */
             id="username"
             type="text"
-            onChange={userNameInputHandler}
+            ref={nameInputRef} 
           ></input>
           <label htmlFor="userage">Age (Years)</label>
           <input
-            value={age}
             id="userage"
             type="number"
             min="1"
-            onChange={ageInputHandler}
+            /* onChange={ageInputHandler} Taken out when we used ref-s instead
+            value={age} */
+            ref={ageInputRef}
           ></input>
           <button type="submit">Add User</button>
         </form>
